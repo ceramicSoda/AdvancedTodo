a989db61deb7262ab5ca1c2ac700799b73de6970
ceramicSoda <dmch.d89@gmail.com>
2022-07-22 00:25:05 +0300

    styles update

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
+import './index.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import { Provider } from 'react-redux'
 import store from './store/index'
-import './reset.css'
-import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
